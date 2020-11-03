72c37ec7957d05c38b81b919cb5f5a72ca963653
Chekebreek <shura.dushkin@yandex.ru>
2020-11-03 23:24:09 +0500

    Душкин Александр. MASTER

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -4,8 +4,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class Task01Main {
-    //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-    // например вот так:
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
 
         /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
